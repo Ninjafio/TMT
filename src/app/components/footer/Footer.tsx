a56debd428ff22a33f5ea99333d8f50b5c407e81
Art Perch <art.perchatkin@gmail.com>
2025-08-05 19:05:22 +0500

    add burger menu + adaptive footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Logo from 'public/LogoFuta.svg'
@@ -5,10 +7,18 @@ import UpArrow from 'public/UpArrow.svg'
 import "./Footer.scss"
 
 const Footer = () => {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+
   return (
     <div className='Footer'>
       <div className="Footer_wrapper">
-      <Image src={Logo} alt='TMT'></Image>
+      <Image src={Logo} alt='TMT' className='logoFuta'></Image>
       <ul className='Footer__list'>
         <li>Контакты</li>
         <li className='Footer__list_phone'>+7 985 (467) 16-29</li>
@@ -19,7 +29,7 @@ const Footer = () => {
         <li>Адрес</li>
         <li className='Footer__list_address'>129344, г. Москва, вн. тер. г. муниципальный округ Бабушкинский, ул. Верхоянская, д. 18, к. 2, помещ. 2П</li>
       </ul>
-      <Image src={UpArrow} alt='ToTop' className='ToTop'></Image>
+      <Image src={UpArrow} alt='ToTop' className='ToTop' onClick={() => scrollToTop()}></Image>
       </div>
       <p className='Footer_bottom-sign'>OOO “TMT” 2017-2025 - ремонт машин и оборудования</p>
     </div>
